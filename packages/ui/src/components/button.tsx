b595d8e25d9aaba168a0e530dbb58105936e570a
Vitor Sousa <v.sousa.cf@gmail.com>
2024-12-14 16:41:22 -0300

    feat: :sparkles: add tailwind and postcss

---
+++ packages/ui/src/components/button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import type { ReactNode } from "react";
+
+interface ButtonProps {
+	children: ReactNode;
+	className?: string;
+	appName: string;
+}
+
+export const Button = ({ children, className, appName }: ButtonProps) => {
+	return (
+		<button
+			className={className}
+			type="button"
+			style={{ backgroundColor: "blue" }}
+			onClick={() => alert(`Hello from your ${appName} app!`)}
+		>
+			{children}
+		</button>
+	);
+};
