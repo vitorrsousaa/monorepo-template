5d1b4657fa03d881302c4586731473a2e9acdc4b
Vitor Sousa <v.sousa.cf@gmail.com>
2024-12-23 19:42:42 -0300

    fix(ui): :sparkles: update icons

--- packages/ui/src/components/sheet.tsx
+++ packages/ui/src/components/sheet.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import * as React from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
-import { cva, type VariantProps } from "class-variance-authority";
-import { X } from "lucide-react";
+import { type VariantProps, cva } from "class-variance-authority";
+import * as React from "react";
 
 import { cn } from "@/utils";
 
@@ -65,7 +65,7 @@ const SheetContent = React.forwardRef<
 			{...props}
 		>
 			<SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-				<X className="h-4 w-4" />
+				<XMarkIcon className="h-4 w-4" />
 				<span className="sr-only">Close</span>
 			</SheetPrimitive.Close>
 			{children}
@@ -128,13 +128,13 @@ SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
 export {
 	Sheet,
-	SheetPortal,
-	SheetOverlay,
-	SheetTrigger,
 	SheetClose,
 	SheetContent,
-	SheetHeader,
-	SheetFooter,
-	SheetTitle,
 	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetOverlay,
+	SheetPortal,
+	SheetTitle,
+	SheetTrigger,
 };
