07dd0543b5368b7d39e67daa9c99894fc508d253
Vitor Sousa <v.sousa.cf@gmail.com>
2026-01-04 15:18:10 -0300

    chore(ui): remove unused "use client" directive from sidebar component

--- packages/ui/src/components/sidebar.tsx
+++ packages/ui/src/components/sidebar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Slot } from "@radix-ui/react-slot";
 import { SidebarLeftIcon } from "@sidekickicons/react/24/outline";
 import { cva, type VariantProps } from "class-variance-authority";
