bbfca2d9abea473a307457ebcf2496aa37d208f7
Vitor Sousa <v.sousa.cf@gmail.com>
2026-01-04 14:58:48 -0300

    fix(ui): fix linter errors

--- packages/ui/src/components/sheet.tsx
+++ packages/ui/src/components/sheet.tsx
@@ -51,7 +51,7 @@ const sheetVariants = cva(
 
 interface SheetContentProps
 	extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-	VariantProps<typeof sheetVariants> { }
+		VariantProps<typeof sheetVariants> {}
 
 const SheetContent = React.forwardRef<
 	React.ElementRef<typeof SheetPrimitive.Content>,
@@ -136,5 +136,5 @@ export {
 	SheetOverlay,
 	SheetPortal,
 	SheetTitle,
-	SheetTrigger
+	SheetTrigger,
 };
