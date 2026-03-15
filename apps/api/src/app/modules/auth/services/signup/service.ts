7d654a23c17323d24419ef5f607ae91d60d01b61
Vitor Sousa <v.sousa.cf@gmail.com>
2026-03-15 19:59:55 -0300

    feat(api): extend SignupService to include user creation in the signup process and add user repository interface

--- apps/api/src/app/modules/auth/services/signup/service.ts
+++ apps/api/src/app/modules/auth/services/signup/service.ts
@@ -1,11 +1,12 @@
 import type { IService } from "@application/interfaces/service";
 import { IAuthProvider } from "@data/protocols/auth/auth-provider";
+import { IUserRepository } from "@data/protocols/auth/user-repository";
 import { SignupInput, SignupOutput } from "./dto";
 
 export interface ISignupService extends IService<SignupInput, SignupOutput> {}
 
 export class SignupService implements ISignupService {
-	constructor(private readonly authProvider: IAuthProvider) {}
+	constructor(private readonly authProvider: IAuthProvider, private readonly userRepository: IUserRepository) {}
 
 	async execute(data: SignupInput): Promise<SignupOutput> {
 		const result = await this.authProvider.signUp({
@@ -14,7 +15,13 @@ export class SignupService implements ISignupService {
 			email: data.email,
 			password: data.password,
 		});
-
+		
+		await this.userRepository.create({
+			id: result.userId,
+			email: data.email,
+			name: `${data.firstName} ${data.lastName}`,
+		});
+		
 		return {
 			userId: result.userId,
 		};
