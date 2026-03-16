a76cecde3e59f95be6df3596a9e808d5ea40c61e
Vitor Sousa <v.sousa.cf@gmail.com>
2026-03-15 22:37:53 -0300

    feat(api): introduce DatabaseTable class and factory function for dynamic table name generation based on environment configuration

---
+++ apps/api/src/app/config/tables.ts
@@ -0,0 +1,13 @@
+import { IConfig } from "./environment";
+
+export class DatabaseTable implements IDatabaseTable {
+  constructor(private readonly config: IConfig) {}
+  
+  public get TABLE_NAME(): string {
+    return `${this.config.SERVICE}-${this.config.STAGE}`;
+  }
+}
+
+export interface IDatabaseTable {
+  TABLE_NAME: string;
+}
---
+++ apps/api/src/factories/database-table.ts
@@ -0,0 +1,7 @@
+import { DatabaseTable, IDatabaseTable } from "@application/config/tables";
+import { makeConfig } from "./config";
+
+export function makeDatabaseTable(): IDatabaseTable {
+  const config = makeConfig();
+  return new DatabaseTable(config);
+}
